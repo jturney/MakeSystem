f2df087b8bab0556aac81e85e2d31811137a7698
Justin Turney <justin.turney@gmail.com>
2012-12-06 14:14:50 -0500

    Adds the ability to list multiple binaries to be built.

--- Makefile
+++ Makefile
@@ -1,17 +1,25 @@
-exe := executable_name
-sources := main.cc
+# List of binaries to generate
+exe := etemplate dimension
+
+# Sources specific to each binary
+etemplate_sources := src/main.cc
+dimension_sources := src/dimension.cc
+
+# Common include directories
 include_dirs := include
 
 # Override the default compiler
 #    Understands clang++, g++, and icpc.
 CXX := clang++
 
-# Other things that I could need:
+# Other things that I could need. These flags apply for all binaries
 #    need_debug := adds debug flags to the compilation
-#    need_blas := true includes BLAS and LAPACK
+#    need_blas  := true includes BLAS and LAPACK
 #    need_boost := true includes Boost and libraries defined in settings.mk/boost.mk
+#    need_gmp   := true includes The GNU Multiple Precision Arithmetic Library
 #need_blas := false
 need_debug := true
+need_gmp := true
 need_boost := true
 
 include common.mk
--- common.mk
+++ common.mk
@@ -1,9 +1,6 @@
-assembly := $(subst .cc,.s,$(sources))
-objects := $(subst .cc,.o,$(sources))
-dependencies := $(subst .cc,.d,$(sources))
-
 include settings.mk/$(CXX).mk
 include settings.mk/boost.mk
+include settings.mk/gmp.mk
 
 CXXFLAGS += $($(CXX).cxxflags)
 LDFLAGS  += $($(CXX).ldflags)
@@ -25,15 +22,31 @@ ifeq "$(need_boost)" "true"
 	LIBRARIES += $(boost.libraries)
 endif
 
-CPPFLAGS += $(addprefix -I ,$(include_dirs))
+ifeq "$(need_gmp)" "true"
+	include_dirs += $(gmp.include_dirs)
+	LIBRARIES += $(gmp.libraries)
+endif
+
+CPPFLAGS += $(addprefix -I ,$(sort $(include_dirs)))
 vpath %.h $(include_dirs)
+
+define build-program
+$1_objects := $(subst .cc,.o,$2)
+$1_dependencies := $(subst .cc,.d,$2)
+$1_assembly := $(subst .cc,.s,$2)
+objects += $$($1_objects)
+dependencies += $$($1_dependencies)
+assembly += $$($1_assembly)
+
+$1: $$($1_objects)
+	$(CXX) $(LDFLAGS) -o $$@ $$^ $(LIBRARIES)
+endef
+
+#$(eval $(call build-program,etemplate,$(etemplate_sources)))
+$(foreach prog,$(exe),$(eval $(call build-program,$(prog),$($(prog)_sources))))
 
 # Default make target
 all: $(exe)
-
-# Rule to create executable
-$(exe): $(objects)
-	$(CXX) $(LDFLAGS) -o $@ $^ $(LIBRARIES)
 
 # Rule to create assembly code only
 asm: $(assembly)
